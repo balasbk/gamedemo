b0096a2a846737488c98b756f43c20dfc30a7179
Bala Kumar S <44743137+balasbk@users.noreply.github.com>
2019-09-03 20:53:45 +0530

    Add files via upload

---
+++ Assets/EventListner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListner : MonoBehaviour
+{
+    public Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer> ();
+    }
+
+    public void Colorchange()
+    {
+        if (_renderer.material.color == Color.white)
+            _renderer.material.color = Color.red;
+        else
+            _renderer.material.color = Color.white;
+    }
+
+    public void dest()
+    {
+        Destroy(gameObject);
+    }
+}
